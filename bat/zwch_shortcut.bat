fcbe2ec3ff001b1e3cf6b30e9f0f0238f2cb6054
simplified-un-renard-en-feu <83009811+simplified-un-renard-en-feu@users.noreply.github.com>
2021-04-23 18:59:35 +0900

    Update zwch_shortcut.bat

--- bat/zwch_shortcut.bat
+++ bat/zwch_shortcut.bat
@@ -1,9 +1,41 @@
 @echo off
 
-rem
-rem creates shortcut for zwch.bat
-rem
+echo ===============================
+echo  create shortcut for zwch.bat
+echo ===============================
 
-set ROOMID=%1
-set PWDHASH=%2
+set LNKNAME=%1
+set ROOMID=%2
+set PWDHASH=%3
+
+:lnkname
+IF "%1" EQU "" (
+set /P LNKNAME=Input Shortcut Name^(^\,^/,^*,^?,^",^<,^>,^| are invalid^):
+)
+IF "%LNKNAME%" EQU "" ( goto :lnkname )
+rem echo '%LNKNAME%' | find \" >NUL
+rem if not ERRORLEVEL 1 ( echo error: invalid character^(^\,^/,^*,^?,^",^<,^>,^|^) & goto :lnkname)
+echo '%LNKNAME%' | find "\" >NUL
+if not ERRORLEVEL 1 ( echo error: invalid character^(^\,^/,^*,^?,^",^<,^>,^|^) & goto :lnkname)
+echo '%LNKNAME%' | find "/" >NUL
+if not ERRORLEVEL 1 ( echo error: invalid character^(^\,^/,^*,^?,^",^<,^>,^|^) & goto :lnkname)
+echo '%LNKNAME%' | find "*" >NUL
+if not ERRORLEVEL 1 ( echo error: invalid character^(^\,^/,^*,^?,^",^<,^>,^|^) & goto :lnkname)
+echo '%LNKNAME%' | find "?" >NUL
+if not ERRORLEVEL 1 ( echo error: invalid character^(^\,^/,^*,^?,^",^<,^>,^|^) & goto :lnkname)
+echo '%LNKNAME%' | find "<" >NUL
+if not ERRORLEVEL 1 ( echo error: invalid character^(^\,^/,^*,^?,^",^<,^>,^|^) & goto :lnkname)
+echo '%LNKNAME%' | find ">" >NUL
+if not ERRORLEVEL 1 ( echo error: invalid character^(^\,^/,^*,^?,^",^<,^>,^|^) & goto :lnkname)
+echo '%LNKNAME%' | find "|" >NUL
+if not ERRORLEVEL 1 ( echo error: invalid character^(^\,^/,^*,^?,^",^<,^>,^|^) & goto :lnkname)
+set LNKNAME=%LNKNAME%.lnk
+
+IF "%2" EQU "" ( set /P ROOMID="Input Meeting ID:" )
+IF "%3" EQU "" ( set /P PWDHASH="Input Password Hash (press enter to skip):" )
+
+set LNKARG=/c .\zwc.bat "%ROOMID%" "%PWDHASH%"
+
+powershell -Command "& {$WshShell = New-Object -comObject WScript.Shell;$Shortcut = $WshShell.CreateShortcut('%LNKNAME%');$Shortcut.TargetPath = 'C:\Windows\system32\cmd.exe ';$Shortcut.Arguments = '%LNKARG%';$Shortcut.Save();}"
+timeout 3
 
