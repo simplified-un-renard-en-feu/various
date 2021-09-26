1adc72b8cec8f3a8912a4940577374734025de94
simplified-un-renard-en-feu <83009811+simplified-un-renard-en-feu@users.noreply.github.com>
2021-09-26 15:10:46 +0900

    Update zwch_shortcut.bat
    fix

--- bat/zwch_shortcut.bat
+++ bat/zwch_shortcut.bat
@@ -13,14 +13,15 @@ set PWDHASH=%4
 
 cd %~dp0
 
-echo absolute path mode: This able to move "zwch.bat" but must execute link in the same folder with "zwch.bat".
-echo relative path mode: This able to execute and move link anywhere but can't move "zwch.bat".
+echo absolute path mode: able to execute and move link anywhere but can't move zwch.bat
+echo relative path mode: able to move the parent folder but must execute link in the same folder with zwch.bat
 echo;
 :isabsolute
 IF "%2" EQU "" (
-set /P ISABSOLUTE=Select Mode^(0^: absolute path mode, 1^: relative path mode^):
+set /P ISABSOLUTE=Select Mode^(0^: absolute path mode, 1^: relative path mode^)[0]:
 )
 IF "%ISABSOLUTE%" EQU "0" ( set BATCHDIR=%~dp0
+) ELSE IF "%ISABSOLUTE%" EQU "" ( set BATCHDIR=%~dp0
 ) ELSE IF "%ISABSOLUTE%" EQU "1" ( set BATCHDIR=.\
 ) ELSE (goto :isabsolute)
 
